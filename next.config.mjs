a7412260d0b1e9880d0987ec95445ce588395d54
thaihung204 <hunghbtde180329@fpt.edu.vn>
2026-01-27 12:32:19 +0700

    Add docer file

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   // output: 'export', // Temporarily disable static export for i18n testing
 
   // Specify the path if your app is not deployed at the root of your domain.
